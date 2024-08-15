495c7eb9d94e22fe57abc2be342905602ccb7ed4
racheltasolyi <43007914+racheltasolyi@users.noreply.github.com>
2024-08-15 13:44:44 -0400

    First commit

---
+++ cogs/reddit.py
@@ -0,0 +1,71 @@
+import discord
+from discord.ext import commands
+from random import choice
+import asyncpraw as praw
+
+class Reddit(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.reddit = praw.Reddit(client_id="VEf7ZU_hPNDG1HeStFWcrg", client_secret="iWo95WMxm8ciD9guFJlxhYpmTk5qcA", user_agent="script:randommeme:v1.0 (by u/SoulDai23)")
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{__name__} is ready!")
+    
+    @commands.command(aliases=["memes"])
+    async def meme(self, ctx: commands.Context):
+
+        subreddit = await self.reddit.subreddit("memes")
+        posts_list = []
+
+        async for post in subreddit.hot(limit=30):
+            if not post.over_18 and post.author is not None and any(post.url.endswith(ext) for ext in [".png", ".jpg", ".jpeg", ".gif"]):
+                author_name = post.author.name
+                posts_list.append((post.url, author_name))
+            if post.author is None:
+                posts_list.append((post.url, "N/A"))
+        
+        if posts_list:
+
+            random_post = choice(posts_list)
+
+            meme_embed = discord.Embed(title="Random Meme", description="Fetches random meme from r/memes", color=discord.Color.random())
+            meme_embed.set_author(name=f"Meme requested by {ctx.author.name}", icon_url=ctx.author.avatar)
+            meme_embed.set_image(url=random_post[0])
+            meme_embed.set_footer(text=f"Post created by {random_post[1]}.", icon_url=None)
+            await ctx.send(embed=meme_embed)
+        
+        else:
+            await ctx.send("Unable to fetch meme, try again later.")
+    
+    @commands.command(aliases=["cats", "catpic", "catt"])
+    async def cat(self, ctx: commands.Context):
+
+        subreddit = await self.reddit.subreddit("cats")
+        posts_list = []
+
+        async for post in subreddit.hot(limit=30):
+            if not post.over_18 and post.author is not None and any(post.url.endswith(ext) for ext in [".png", ".jpg", ".jpeg", ".gif"]):
+                author_name = post.author.name
+                posts_list.append((post.url, author_name))
+            if post.author is None:
+                posts_list.append((post.url, "N/A"))
+        
+        if posts_list:
+
+            random_post = choice(posts_list)
+
+            meme_embed = discord.Embed(title="Cat Pic", description="Fetches a random cat from r/cat", color=discord.Color.random())
+            meme_embed.set_author(name=f"Cat requested by {ctx.author.name}", icon_url=ctx.author.avatar)
+            meme_embed.set_image(url=random_post[0])
+            meme_embed.set_footer(text=f"Post created by {random_post[1]}.", icon_url=None)
+            await ctx.send(embed=meme_embed)
+        
+        else:
+            await ctx.send("Unable to fetch cat, try again later.")
+    
+    def cog_unload(self):
+        self.bot.loop.create_task(self.reddit.close())
+    
+async def setup(bot):
+    await bot.add_cog(Reddit(bot))
---
+++ main.py
@@ -0,0 +1,59 @@
+import discord
+from discord.ext import commands, tasks
+import os
+import asyncio
+from itertools import cycle
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+bot_statuses = cycle(["Status One", "Status Two", "Status Three", "Status Four"])
+
+@tasks.loop(seconds=2)
+async def change_bot_status():
+    await bot.change_presence(activity=discord.Game(next(bot_statuses)))
+
+@bot.event
+async def on_ready():
+    print("Bot ready!")
+    change_bot_status.start()
+
+@bot.command(aliases=["hi"])
+async def hello(ctx):
+    await ctx.send(f"Hello there, {ctx.author.mention}!")
+
+@bot.command(aliases=["gm", "morning"])
+async def goodmorning(ctx):
+    await ctx.send(f"Good morning, {ctx.author.mention}!")
+
+@bot.command()
+async def sendembed(ctx):
+    embeded_msg = discord.Embed(title="Title of embed", description="Description of embed", color=discord.Color.green())
+    embeded_msg.set_author(name="Author text", icon_url=ctx.author.avatar)
+    embeded_msg.set_thumbnail(url=ctx.author.avatar)
+    embeded_msg.add_field(name="Name of field", value="Value of field", inline=False)
+    embeded_msg.set_image(url=ctx.guild.icon)
+    embeded_msg.set_footer(text="Footer text", icon_url=ctx.author.avatar)
+    await ctx.send(embed=embeded_msg)
+
+# @bot.command()
+# async def ping(ctx):
+#     ping_embed = discord.Embed(title="Ping",description="Latency in ms", color=discord.Color.blue())
+#     ping_embed.add_field(name=f"{bot.user.name}'s Latency: ", value=f"{round(bot.latency * 1000)}ms.", inline=False)
+#     ping_embed.set_footer(text=f"Requested by {ctx.author.name}.", icon_url=ctx.author.avatar)
+#     await ctx.send(embed=ping_embed)
+
+async def Load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+async def main():
+    async with bot:
+        await Load()
+        await bot.start(token)
+
+with open("token.txt") as file:
+    token = file.read()
+
+asyncio.run(main())
+#bot.run(token)
