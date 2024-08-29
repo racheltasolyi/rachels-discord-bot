e3c14c444483a92e6d4afbc867bd6d960c40cdcb
racheltasolyi <43007914+racheltasolyi@users.noreply.github.com>
2024-08-28 21:54:29 -0400

    added level system: Leveling

---
+++ cogs/leveling.py
@@ -0,0 +1,81 @@
+import discord
+from discord.ext import commands
+import sqlite3
+import math
+import random
+
+class Leveling(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{__name__} is online!")
+    
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+        
+        connection = sqlite3.connect("./cogs/levels.db")
+        cursor = connection.cursor()
+        guild_id = message.guild.id
+        user_id = message.author.id
+
+        cursor.execute("SELECT * FROM Users WHERE guild_id = ? AND user_id = ?", (guild_id, user_id))
+
+        result = cursor.fetchone()
+
+        if result is None:
+            cur_level = 0
+            xp = 0
+            level_up_xp = 100
+            cursor.execute("INSERT INTO Users (guild_id, user_id, level, xp, level_up_xp) Values (?,?,?,?,?)", (guild_id, user_id, cur_level, xp, level_up_xp))
+
+        else:
+            cur_level = result[2]
+            xp = result[3]
+            level_up_xp = result[4]
+
+            xp += random.randint(1, 25)
+
+        if xp >= level_up_xp:
+            cur_level += 1
+            new_level_up_xp = math.ceil(50 * cur_level ** 2 + 100 * cur_level + 50)
+
+            await message.channel.send(f"{message.author.mention} has leveled up to level {cur_level}!")
+
+            cursor.execute("UPDATE Users SET level = ?, xp = ?, level_up_xp = ? WHERE guild_id = ? AND user_id = ?", (cur_level, xp, new_level_up_xp, guild_id, user_id))
+
+        cursor.execute("UPDATE Users SET xp = ? WHERE guild_id = ? AND user_id = ?", (xp, guild_id, user_id))
+
+        connection.commit()
+        connection.close()
+
+    @commands.command()
+    async def level(self, ctx: commands.Context, member: discord.Member=None):
+
+        if member is None:
+            member = ctx.author
+        
+        member_id = member.id
+        guild_id = ctx.guild.id
+
+        connection = sqlite3.connect("./cogs/levels.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM Users WHERE guild_id = ? AND user_id = ?", (guild_id, member_id))
+        result = cursor.fetchone()
+
+        if result is None:
+            await ctx.send(f"{member.name} currently does not have a level.")
+        else:
+            level = result[2]
+            xp = result[3]
+            level_up_xp = result[4]
+            
+            await ctx.send(f"Level Statistics for {member.name}:\nLevel: {level}\nXP: {xp}\nXP To Level Up: {level_up_xp}")
+
+        connection.close()
+
+async def setup(bot):
+    await bot.add_cog(Leveling(bot))
