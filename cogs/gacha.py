229669d83909c45d63543f0edf870f5e8ab4d05d
racheltasolyi <43007914+racheltasolyi@users.noreply.github.com>
2024-09-04 16:46:53 -0400

    !gacha now displays Lee Know card, started JSON for storing cards

--- cogs/gacha.py
+++ cogs/gacha.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import os
+import random
 
 class Gacha(commands.Cog):
     def __init__(self, bot):
@@ -18,13 +20,20 @@ class Gacha(commands.Cog):
 
     @commands.command(aliases=["g"])
     async def gacha(self, ctx):
-        card = discord.Embed(title="Title of embed", description="Description of embed", color=discord.Color.green())
-        card.set_author(name="Author text", icon_url=ctx.author.avatar)
-        card.set_thumbnail(url=ctx.author.avatar)
-        card.add_field(name="Name of field", value="Value of field", inline=False)
-        card.set_image(url=ctx.guild.icon)
-        card.set_footer(text="Footer text", icon_url=ctx.author.avatar)
-        await ctx.send(embed=card)
+        #images = [image for image in os.listdir("./cogs/welcome_images")]
+        #randomized_image = random.choice(images)
+
+        welcome1 = discord.File("./cogs/welcome_images/welcome1.jpg", filename="welcome1.jpg")
+        skzlogo = discord.File("./cogs/welcome_images/skzlogo.jpg", filename="skzlogo.jpg")
+
+        card = discord.Embed(title="Lee Know", description="Stray Kids", color=discord.Color.green())
+        card.set_author(name=f"Rolled by {ctx.author.name}", icon_url=ctx.author.avatar)
+        card.set_thumbnail(url="attachment://skzlogo.jpg")
+        #card.add_field(name="Name of field", value="Value of field", inline=False)
+        card.set_image(url="attachment://welcome1.jpg")
+        #card.set_image(f"./cogs/welcome_images/{randomized_image}")
+        card.set_footer(text="Lee Know has not been caught yet!")
+        await ctx.send(files=[welcome1, skzlogo], embed=card)
 
 async def setup(bot):
     await bot.add_cog(Gacha(bot))
