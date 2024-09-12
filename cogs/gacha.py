48ea7aeb2516bbf28acc5c7558fc70472d5eea71
racheltasolyi <43007914+racheltasolyi@users.noreply.github.com>
2024-09-11 22:52:51 -0400

    users can now permanently catch idols

--- cogs/gacha.py
+++ cogs/gacha.py
@@ -24,13 +24,13 @@ class Gacha(commands.Cog):
         #images = [image for image in os.listdir("./cogs/welcome_images")]
         #randomized_image = random.choice(images)
 
-        roll = random.randrange(3)
+        roll_number = random.randrange(3)
         cards = await self.get_card_data()
-        roll_name = cards[str(roll)]["name"]
-        roll_group = cards[str(roll)]["group"]
-        roll_image = cards[str(roll)]["image"]
-        roll_logo = cards[str(roll)]["logo"]
-        roll_claimed = cards[str(roll)]["claimed"]
+        roll_name = cards[str(roll_number)]["name"]
+        roll_group = cards[str(roll_number)]["group"]
+        roll_image = cards[str(roll_number)]["image"]
+        roll_logo = cards[str(roll_number)]["logo"]
+        roll_claimed = cards[str(roll_number)]["claimed"]
         #print(roll_name)
 
         uploaded_roll_image = discord.File(f"./cogs/gacha_images/{roll_image}", filename=roll_image)
@@ -42,7 +42,7 @@ class Gacha(commands.Cog):
         card.set_footer(text=f"Rolled by {ctx.author.name}", icon_url=ctx.author.avatar)
 
         if roll_claimed:
-            roll_owner_id = cards[str(roll)]["owner"]
+            roll_owner_id = cards[str(roll_number)]["owner"]
             roll_owner = await ctx.bot.fetch_user(roll_owner_id)
             #print(roll_owner_id)
             #print(roll_owner)
@@ -51,44 +51,94 @@ class Gacha(commands.Cog):
                 value=f"{roll_name} can no longer be caught.",
                 inline=False
             )
+            await ctx.send(files=[uploaded_roll_image, uploaded_roll_logo], embed=card)
         else:
             card.add_field(
                 name=f"{roll_name} has not been caught yet 🥺",
-                value="React with any emoji to claim!",
+                value="Throw a Pokeball to catch!",
                 inline=False
             )
-        
-        await ctx.send(files=[uploaded_roll_image, uploaded_roll_logo], embed=card, view=TestMenuButton())
-    
+            await ctx.send(files=[uploaded_roll_image, uploaded_roll_logo], embed=card, view=GachaButtonMenu(roll_number))
+            
     '''
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
         channel = reaction.message.channel
         await client.send_message(channel, "{} has added {} to the message: {}")
     '''
+    
+    async def create_new_player(self, player):
+        #print("Do we need to open an account?")
+        players = await self.get_player_data()
+        
+        if str(player.id) in players:
+            #print("No, account already exists.")
+            return False
+        else:
+            #print(f"Yes, creating account for {user.id}...")
+            players[str(player.id)] = {}
+            players[str(player.id)]["party"] = {}
+            players[str(player.id)]["bank"] = 0
+            #print(f"{user.id}'s account was created.")
+
+        await self.update_player_data(players)
+        return True
+    
+    async def get_player_data(self):
+        #print("Getting bank data...")
+        with open("./cogs/gachaplayers.json","r") as f:
+            #print("Reading JSON...")
+            players = json.load(f)
+
+        #print("JSON read!")
+        return players
+    
+    async def update_player_data(self, players):
+        with open("./cogs/mainbank.json","w") as f:
+            #print("Updating JSON...")
+            json.dump(players,f)
+
+        #print("JSON updated!")
+        return True
+    
     async def get_card_data(self):
         #print("Getting card data...")
-        with open("./cogs/gacha.json","r") as f:
+        with open("./cogs/gachacards.json","r") as f:
             #print("Reading JSON...")
             cards = json.load(f)
 
         #print("JSON read!")
         return cards
     
-class TestMenuButton(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
+    async def update_card_data(self, cards):
+        with open("./cogs/gachacards.json","w") as f:
+            #print("Updating JSON...")
+            json.dump(cards,f)
+
+        #print("JSON updated!")
+        return True
     
-    @discord.ui.button(label="Test", style=discord.ButtonStyle.blurple)
+class GachaButtonMenu(discord.ui.View):
+    roll_number = None
+
+    def __init__(self, roll_number):
+        super().__init__(timeout=None)
+        self.roll_number = roll_number
+    
+    @discord.ui.button(label="Throw Pokeball", style=discord.ButtonStyle.blurple)
     async def test(self, interaction: discord.Interaction, Button: discord.ui.Button):
-        await interaction.response.send_message(content="Test successful!")
-        self.stop()
-    @discord.ui.button(label="Click Me...", style=discord.ButtonStyle.green)
-    async def test2(self, interaction: discord.Interaction, Button: discord.ui.Button):
-        await interaction.response.send_message(content="I've been clicked!")
-    @discord.ui.button(label="Exit", style=discord.ButtonStyle.red)
-    async def test3(self, interaction: discord.Interaction, Button: discord.ui.Button):
-        await interaction.response.send_message(content="Exiting Menu...")
+        userid = interaction.user.id
+        #print(userid)
+        cards = await Gacha.get_card_data(self)
+        #print(cards)
+        #print(self.roll_number)
+        #print(f"before: {cards[str(self.roll_number)]["claimed"]}")
+        cards[str(self.roll_number)]["claimed"] = True
+        cards[str(self.roll_number)]["owner"] = userid
+        #print(f"after: {cards[str(self.roll_number)]["claimed"]}")
+        #print(cards[str(self.roll_number)]["owner"])
+        await Gacha.update_card_data(self, cards)
+        await interaction.response.send_message(content=f"{cards[str(self.roll_number)]["name"]} was caught by {interaction.user.name}!")
 
 async def setup(bot):
     await bot.add_cog(Gacha(bot))
