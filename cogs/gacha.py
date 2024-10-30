13f829af907706eafd89802c378ec40f76328dd0
racheltasolyi <43007914+racheltasolyi@users.noreply.github.com>
2024-10-30 17:07:12 -0400

    created and connected SQLite database for gacha game

--- cogs/gacha.py
+++ cogs/gacha.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import random
 import json
+import sqlite3
 
 class Gacha(commands.Cog):
     def __init__(self, bot):
@@ -16,30 +17,70 @@ class Gacha(commands.Cog):
         #images = [image for image in os.listdir("./cogs/welcome_images")]
         #randomized_image = random.choice(images)
         #print("!gacha command called!")
+        roller_id = ctx.author.id
+        #print(roller_id)
 
-        roll_number = random.randrange(3)
+        roll_number = random.randrange(2)
         #print(roll_number)
-        cards = await self.get_card_data()
+        #cards = await self.get_card_data()
+        connection = sqlite3.connect("./cogs/idol_gacha.db")
+        cursor = connection.cursor()
+        #print("connection made")
+
+        cursor.execute("""SELECT * FROM Players
+                       WHERE player_id = :roller_id""",
+                       {'roller_id': roller_id})
+        player = cursor.fetchone()
+        #print(player)
+        if player is None:
+            cursor.execute("""INSERT INTO Players (player_id)
+                           Values (:roller_id)""",
+                           {'roller_id': roller_id})
+
+        cursor.execute("""SELECT * FROM Idols
+                       WHERE idol_id = :roll_number""",
+                       {'roll_number': roll_number})
+        roll = cursor.fetchone()
+        '''if roll is not None:
+            print(roll)
+        else:
+            print("No matching entry found.")'''
+
+        cursor.execute("""SELECT group_id FROM GroupMembers
+                       WHERE idol_id = :roll_number""",
+                       {'roll_number': roll_number})
+        roll_group_id = cursor.fetchone()
+        #print(roll_group_id)
+
+        cursor.execute("""SELECT * FROM Groups
+                       WHERE group_id = :roll_group_id""",
+                       {'roll_group_id': roll_group_id[0]})
+        roll_group = cursor.fetchone()
+        #print(roll_group)
+
         #print("Got card data!")
-        roll_name = cards[str(roll_number)]["name"]
-        roll_group = cards[str(roll_number)]["group"]
-        roll_image = cards[str(roll_number)]["image"]
-        roll_logo = cards[str(roll_number)]["logo"]
-        roll_claimed = cards[str(roll_number)]["claimed"]
+        roll_name = roll[1]
+        roll_group_name = roll_group[1]
+        roll_image = roll[2]
+        roll_logo = roll_group[2]
+        if (roll[3] == 0 or roll[3] == None):
+            roll_claimed = False
+        else:
+            roll_claimed = True
         #print(roll_name)
 
         uploaded_roll_image = discord.File(f"./cogs/gacha_images/idols/{roll_image}", filename=roll_image)
         uploaded_roll_logo = discord.File(f"./cogs/gacha_images/logos/{roll_logo}", filename=roll_logo)
         #print("Images uploaded!")
 
-        card = discord.Embed(title=roll_name, description=roll_group, color=discord.Color.green())
+        card = discord.Embed(title=roll_name, description=roll_group_name, color=discord.Color.green())
         card.set_thumbnail(url=f"attachment://{roll_logo}")
         card.set_image(url=f"attachment://{roll_image}")
         card.set_footer(text=f"Rolled by {ctx.author.name}", icon_url=ctx.author.avatar)
         #print("Embed created!")
 
         if roll_claimed:
-            roll_owner_id = cards[str(roll_number)]["owner"]
+            roll_owner_id = roll[3]
             roll_owner = await ctx.bot.fetch_user(roll_owner_id)
             #print(roll_owner_id)
             #print(roll_owner)
@@ -55,7 +96,10 @@ class Gacha(commands.Cog):
                 value="You have 1 minute to throw a Pokeball!",
                 inline=False
             )
+            #await ctx.send(files=[uploaded_roll_image, uploaded_roll_logo], embed=card)
             await ctx.send(files=[uploaded_roll_image, uploaded_roll_logo], embed=card, view=GachaButtonMenu(roll_number))
+        connection.commit()
+        connection.close()
     
     @commands.command()
     async def resetgacha(self, ctx):
@@ -130,17 +174,45 @@ class GachaButtonMenu(discord.ui.View):
     roll_number = None
 
     def __init__(self, roll_number):
-        super().__init__(timeout=3)
+        super().__init__(timeout=60)
         self.roll_number = roll_number
     
     @discord.ui.button(label="Throw Pokeball", style=discord.ButtonStyle.blurple)
     async def test(self, interaction: discord.Interaction, Button: discord.ui.Button):
         userid = interaction.user.id
         #print(userid)
-        cards = await Gacha.get_card_data(self)
+        #cards = await Gacha.get_card_data(self)
         #print(cards)
         #print(self.roll_number)
         #print(f"before: {cards[str(self.roll_number)]['claimed']}")
+        connection = sqlite3.connect("./cogs/idol_gacha.db")
+        cursor = connection.cursor()
+        #print("connection made")
+
+        cursor.execute("""SELECT * FROM Idols
+                       WHERE idol_id = :roll_number""",
+                       {'roll_number': self.roll_number})
+        roll = cursor.fetchone()
+        roll_name = roll[1]
+        #print(roll)
+
+        if (roll[3] == 0 or roll[3] == None):
+            roll_claimed = False
+        else:
+            roll_claimed = True
+        
+        if not roll_claimed:
+            cursor.execute("UPDATE Idols SET player_id = :userid WHERE idol_id = :roll_number",
+                       {'userid': userid, 'roll_number': self.roll_number})
+            content=f"{roll_name} was caught by {interaction.user.mention}!"
+            print(roll)
+        else:
+            content=f"Too bad, {roll_name} has already been caught!"
+        
+        connection.commit()
+        connection.close()
+
+        '''
         if cards[str(self.roll_number)]["claimed"] is False:
             cards[str(self.roll_number)]["claimed"] = True
             cards[str(self.roll_number)]["owner"] = userid
@@ -148,8 +220,10 @@ class GachaButtonMenu(discord.ui.View):
             content=f"{cards[str(self.roll_number)]['name']} was caught by {interaction.user.mention}!"
         else:
             content=f"Too bad, {cards[str(self.roll_number)]['name']} has already been caught!"
+        '''
         #print(f"after: {cards[str(self.roll_number)]['claimed']}")
         #print(cards[str(self.roll_number)]["owner"])
+
         await interaction.response.send_message(content=content)
         
 
